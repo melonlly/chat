cebc2440a45256cb96aa35900fe0b4746509e16d
melonlly <melonlly@outlook.com>
2019-12-19 17:17:36 +0800

    增加拦截器

--- src/controllers/CatController.ts
+++ src/controllers/CatController.ts
@@ -1,13 +1,15 @@
-import { Controller, Get, Query, Param, Post, Body, Delete, UsePipes, UseGuards } from '@nestjs/common';
+import { Controller, Get, Query, Param, Post, Body, Delete, UsePipes, UseGuards, UseInterceptors } from '@nestjs/common';
 import { CatService } from '../services/CatService';
 import { Cat } from 'src/dtos/Cat';
 import { CatValidationPipe } from 'src/pipes/CatValidationPipe';
 import catSchema from 'src/schema/catSchema';
 import { CatGuard } from 'src/guards/CatGuard';
 import { Cats } from 'src/decorators/Cats';
+import { LoggingInterceptor } from 'src/interceptors/LoggingInterceptor';
 
 @Controller('cat')
 @UseGuards(CatGuard)
+@UseInterceptors(LoggingInterceptor)
 export class CatController {
     constructor(private readonly catService: CatService) { }
 
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './module/AppModule';
 import { logger } from './middleware/logger';
 import { GlobalGuard } from './guards/GlobalGuard';
+import { LoggingInterceptor } from './interceptors/LoggingInterceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalGuards(new GlobalGuard())
-  app.use(logger)
+  app.useGlobalGuards(new GlobalGuard()) // 全局守卫
+  // app.useGlobalInterceptors(new LoggingInterceptor()) // 全局拦截器
+  app.use(logger) // 全局中间件
   await app.listen(3000);
 }
 bootstrap();
