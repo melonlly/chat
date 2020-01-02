4ee502b4d98c1d9525d3d11a4094ba63ab924155
melonlly <melonlly@outlook.com>
2020-01-02 17:24:02 +0800

    创建房间和房间内发送接收消息

--- src/events/EventsGateway.ts
+++ src/events/EventsGateway.ts
@@ -9,32 +9,39 @@ import {
 import { from, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Server } from 'socket.io';
+import { uuidv1 } from 'uuid'
 
 @WebSocketGateway()
 export class EventsGateway {
     @WebSocketServer()
     server: Server;
 
+    @SubscribeMessage('create')
+    async createRoom(@MessageBody() params: any, @ConnectedSocket() client: any): Promise<number> {
+        console.log(`创建room参数： ${ JSON.stringify(params) }`)
+        const roomId = params.roomId || client.id // 房间号（唯一），如果请求参数中不包含房间号，则使用客户端id做房间号
+        client.join(roomId, () => {
+            const rooms = Object.keys(client.rooms);
+            console.log(rooms);
+        })
+        return roomId
+    }
+
     @SubscribeMessage('events')
     findAll(@MessageBody() data: any): Observable<WsResponse<number>> {
-        console.log(`events: ${ JSON.stringify(data) }`)
+        console.log(`events: ${JSON.stringify(data)}`)
         return from([1, 2, 3]).pipe(map(item => ({ event: 'events', data: item })));
     }
 
     @SubscribeMessage('identity')
     async identity(@MessageBody() data: number): Promise<number> {
-        console.log(`identity: ${ data }`)
+        console.log(`identity: ${data}`)
         return data;
     }
 
     @SubscribeMessage('send')
-    sendMsg(@MessageBody() msg: string): WsResponse<string> {
-        console.log(`sendMsg: ${ msg }`)
-        this.server.to('melon room').emit('send', '666')
-        return {
-            event: 'send',
-            data: msg
-        }
+    sendMsg(@MessageBody() params: any) {
+        this.server.to(params.roomId).emit('send', params.msg)
     }
 
 }
--- src/gateway/MyIoAdapter.ts
+++ src/gateway/MyIoAdapter.ts
@@ -1,21 +1,47 @@
 import { IoAdapter } from '@nestjs/platform-socket.io';
 import { ServerOptions } from 'socket.io';
+import { MessageMappingProperties } from '@nestjs/websockets';
+import { Observable, fromEvent, EMPTY } from 'rxjs';
+import { mergeMap, filter } from 'rxjs/operators';
 
 export class MyIoAdapter extends IoAdapter {
     createIOServer(port: number, options?: ServerOptions): any {
-        const io = super.createIOServer(port, options);
-        io.on('connection', socket => {
-            console.log('connected')
-            socket.join('melon room', () => {
-                const rooms = Object.keys(socket.rooms);
-                console.log(rooms);
-            });
+        return super.createIOServer(port, options)
+    }
 
-            io.in('melon room').clients((err, clients) => {
-                console.log(clients)
-            })
+    // 绑定客户端连接事件
+    bindClientConnect(server, callback: Function) {
+        server.on('connection', callback);
+    }
 
-        })
-        return io;
+    // // 将传入的消息绑定到适当的消息处理程序
+    // bindMessageHandlers(socket: any, handlers: MessageMappingProperties[], process: (data: any) => Observable<any>) {
+    //     console.log(socket.id)
+    //     fromEvent(socket, 'events')
+    //         .pipe(
+    //             mergeMap(data => this.bindMessageHandler(data, handlers, process)),
+    //             filter(result => result)
+    //         )
+    //         .subscribe(response => socket.send(JSON.stringify(response)));
+    // }
+    // bindMessageHandler(
+    //     data,
+    //     handlers: MessageMappingProperties[],
+    //     process: (data: any) => Observable<any>,
+    // ): Observable<any> {
+    //     const message = data;
+    //     // 查找对应的消息处理
+    //     const messageHandler = handlers.find(
+    //         handler => handler.message === message.event,
+    //     );
+    //     if (!messageHandler) {
+    //         return EMPTY;
+    //     }
+    //     return process(messageHandler.callback(message));
+    // }
+
+    // 终止服务器实例
+    close(server) {
+        server.close();
     }
 }
